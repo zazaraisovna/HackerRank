049fbe47afd296982a73f4952fff064a34bffb5a
Azaliya Khamitova <azaliyaraisovna@gmail.com>
2022-04-21 20:44:13 +0600

    #11 Subarray Division 1

---
+++ Week1/Subarray Division 1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Problem.txt
+     * 
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY s
+     *  2. INTEGER d
+     *  3. INTEGER m
+     */
+
+    public static int birthday(List<int> s, int d, int m)
+    {
+        int cnt = 0;
+        for (int i = 0; i < s.Count; i++)
+        {
+            int scnt = 0;
+            if ((i + m) <= s.Count)
+                for (int j = 0; j < m; j++)
+                {
+                    scnt += s[i + j];
+                }
+            if (scnt == d) cnt++;
+        }
+        return cnt;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> s = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(sTemp => Convert.ToInt32(sTemp)).ToList();
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int d = Convert.ToInt32(firstMultipleInput[0]);
+
+        int m = Convert.ToInt32(firstMultipleInput[1]);
+
+        int result = Result.birthday(s, d, m);
+
+        Console.WriteLine(result);
+    }
+}
