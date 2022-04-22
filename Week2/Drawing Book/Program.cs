cb05a0f25f789e2e70fe8c8f31d43abd00ea89eb
Azaliya Khamitova <azaliyaraisovna@gmail.com>
2022-04-22 17:14:43 +0600

    #14 Drawing Book

---
+++ Week2/Drawing Book/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER p
+     */
+
+    public static int pageCount(int n, int p)
+    {
+        int from_first = (p / 2);
+        int from_past = (n / 2) - (p / 2);
+        if (from_first < from_past)
+            return from_first;
+        else return from_past;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        int p = Convert.ToInt32(Console.ReadLine().Trim());
+
+        int result = Result.pageCount(n, p);
+
+        Console.WriteLine(result);
+    }
+}
