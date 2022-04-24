eab98df88df205609d08d517c464fd0b36397b5d
Azaliya Khamitova <azaliyaraisovna@gmail.com>
2022-04-24 15:51:27 +0600

    #15 Tower Breakers

---
+++ Week2/Tower Breakers/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Input (stdin)
+     * 2
+     * 2 2
+     * 1 4
+     * Expected Output
+     * Download
+     * 2
+     * 1
+     */
+
+    public static int towerBreakers(int n, int m)
+    {
+        if (m == 1) return 2;
+        else if (n % 2 == 1) return 1;
+        else return 2;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int n = Convert.ToInt32(firstMultipleInput[0]);
+
+            int m = Convert.ToInt32(firstMultipleInput[1]);
+
+            int result = Result.towerBreakers(n, m);
+
+            Console.WriteLine(result);
+        }
+    }
+}
