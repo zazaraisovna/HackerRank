887d0f7b5c9ce15ab64d8150e4f7618ffc199d3d
Azaliya Khamitova <azaliyaraisovna@gmail.com>
2022-05-05 16:51:30 +0500

    #17 MaxMin

---
+++ Week2/MaxMin/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static int maxMin(int k, List<int> arr)
+    {
+        k--;
+        arr.Sort();
+        int min = arr[arr.Count - 1];
+        for (int i = 0; i < arr.Count - k; i++)
+        {
+            int tmin = arr[i + k] - arr[i];
+            if (tmin < min) min = tmin;
+        }
+        return min;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        int k = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = new List<int>();
+
+        for (int i = 0; i < n; i++)
+        {
+            int arrItem = Convert.ToInt32(Console.ReadLine().Trim());
+            arr.Add(arrItem);
+        }
+
+        int result = Result.maxMin(k, arr);
+
+        Console.WriteLine(result);
+    }
+}
